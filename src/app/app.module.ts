4f2dbccdb8c74fc6ca5a77ac572ef482a4a64825
Evan Cole <ecole5@uwo.ca>
2018-09-06 12:29:26 -0400

    Basic menu with two components

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+//Here is where we add imports for bootstrap javascript comonpnents
+//WE could cereate another module and then import it into this module
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { AlertModule, CollapseModule } from 'ngx-bootstrap';
+import { AppRoutingModule } from './/app-routing.module';
+import { ProjectsComponent } from './projects/projects.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProjectsComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
+    AlertModule.forRoot(),
+    CollapseModule.forRoot(),
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
