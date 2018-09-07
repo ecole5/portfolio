3d5611525f264f36b687ad00bdd96832e66169ce
Evan Cole <ecole5@uwo.ca>
2018-09-07 16:21:53 -0400

    menu collapse is working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
-//Here is where we add imports for bootstrap javascript comonpnents
-//WE could cereate another module and then import it into this module
+// Here is where we add imports for bootstrap javascript comonpnents
+// WE could cereate another module and then import it into this module
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
-import { AlertModule, CollapseModule } from 'ngx-bootstrap';
+import {ButtonsModule} from 'ngx-bootstrap/buttons';
+import { AlertModule } from 'ngx-bootstrap/alert';
+import {CollapseModule} from 'ngx-bootstrap/collapse';
 import { AppRoutingModule } from './/app-routing.module';
+
 import { ProjectsComponent } from './projects/projects.component';
 import { HomeComponent } from './home/home.component';
 import { ExperienceComponent } from './experience/experience.component';
@@ -32,6 +35,7 @@ import { BlogComponent } from './blog/blog.component';
     ModalModule.forRoot(),
     AlertModule.forRoot(),
     CollapseModule.forRoot(),
+    ButtonsModule.forRoot(),
     AppRoutingModule
   ],
   providers: [],
