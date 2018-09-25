0c920b170ac45f69a2ad4284878e4d08944d2ea0
Evan Cole <ecole5@uwo.ca>
2018-09-24 20:12:21 -0400

    Squashing bugs

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,32 +1,17 @@
-
-import {Component} from '@angular/core';
-
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
-  templateUrl: './projects.component.html'
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.css']
 })
-export class ProjectsComponent {
-  closeResult: string;
+export class ProjectsComponent implements OnInit {
+  isLoggedIn = true;
+  constructor() { }
 
-  constructor(private modalService: NgbModal) {}
-
-  open(content) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
+  ngOnInit() {
   }
 
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return  `with: ${reason}`;
-    }
-  }
+
+
 }
