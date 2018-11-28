9f429172d83600688a212d777e1a70e3898df426
Evan Cole <ecole5@uwo.ca>
2018-11-28 16:08:02 -0500

    isolate angular build, stop dev

--- ember-app/app/helpers/equal.js
+++
@@ -1,7 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-export function equal([first, second, ...theRest]) {
-  return first == second;
-}
-
-export default helper(equal);
--- ember-app/app/helpers/incr.js
+++
@@ -1,8 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-export function incr([number, ...theRest]) {
-
-  return number + 1;
-}
-
-export default helper(incr);
